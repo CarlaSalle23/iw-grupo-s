a6687d28b03630ac54870c9da61a96ed43d5ac70
Alvaro Machaca <amachacam@ulasalle.edu.pe>
2025-04-22 16:55:58 -0500

    Corrigiendo trabajo con apache y virtual host

--- lab06/app.py
+++ lab06/app.py
@@ -4,7 +4,6 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
+    # Asegúrate que la plantilla index.html usa la ruta correcta para el CSS:
+    # /lab06/static/style.css
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
