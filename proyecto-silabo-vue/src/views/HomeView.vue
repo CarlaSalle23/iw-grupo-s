1115409be90d9cb0ba280d8d3f56af93a6421be8
mela0405 <113140005+mela0405@users.noreply.github.com>
2025-05-07 16:07:33 -0500

    Mejora de HomeView

--- proyecto-silabo-vue/src/views/HomeView.vue
+++ proyecto-silabo-vue/src/views/HomeView.vue
@@ -1,9 +1,40 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="home-view">
+    <div class="options">
+      <button @click="navigateTo('abrir')" class="text-hover">Abrir un Sílabo</button>
+      <button @click="navigateTo('editar')" class="text-hover">Editar un Sílabo</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function navigateTo(option) {
+  if (option === 'abrir') {
+    router.push('/abrir-silabo')
+  } else if (option === 'editar') {
+    router.push('/editar-silabo')
+  }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.home-view {
+  text-align: center;
+  margin-top: 50px;
+}
+
+.options {
+  margin-top: 20px;
+}
+
+button {
+  margin: 10px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
