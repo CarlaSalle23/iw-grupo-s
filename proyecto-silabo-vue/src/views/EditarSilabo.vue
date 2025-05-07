3ba04fb4636f2ad684295e5d741d721f678a57ef
mela0405 <113140005+mela0405@users.noreply.github.com>
2025-05-07 16:06:35 -0500

    Create EditarSilabo.vue

---
+++ proyecto-silabo-vue/src/views/EditarSilabo.vue
@@ -0,0 +1,7 @@
+<!-- filepath: d:\proyecto-silabo-vue\src\views\EditarSilabo.vue -->
+<template>
+    <div>
+      <h1>Editar un Sílabo</h1>
+      <p>Funcionalidad para editar un sílabo.</p>
+    </div>
+  </template>
