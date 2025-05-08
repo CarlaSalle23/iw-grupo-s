04af0fd225321b7a642f6036c7b201916a535ac7
mela0405 <113140005+mela0405@users.noreply.github.com>
2025-05-08 16:13:32 +0000

    Actualizacion de nuevo proyecto

--- proyecto-silabo-vue/src/views/EditarSilabo.vue
+++
@@ -1,7 +0,0 @@
-<!-- filepath: d:\proyecto-silabo-vue\src\views\EditarSilabo.vue -->
-<template>
-    <div>
-      <h1>Editar un Sílabo</h1>
-      <p>Funcionalidad para editar un sílabo.</p>
-    </div>
-  </template>
--- proyecto-silabo-vue/src/views/SilabosGuardados.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Abrir un Sílabo</h1>
-    <p>Funcionalidad para abrir un sílabo.</p>
-  </div>
-</template>
